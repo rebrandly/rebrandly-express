1b72bf8301936d7ba275d8d71b14cbfc685b48fa
gianni <gianni@rebrandly.com>
2022-01-14 20:10:35 +0100

    return temporary redirect status code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 const { rewrite } = require("./libs/rewriting");
 const { loopDetected } = require("./libs/integration");
 const { parseOptions } = require("./libs/defaulting");
+const TEMPORARY_REDIRECT = 302;
 
 const setup = (options) => {
     let { log, alias, dryRun, skip } = parseOptions(options); 
-       
+
     const aliasing = (req, res, next) => {
         let nextUrl = rewrite(req, alias);
         if (dryRun) {
             log(`[Rebrandly][DRY] Redirecting to ${nextUrl} ...`)
             return next();
         }
-        return res.redirect(nextUrl);
+        return res.redirect(TEMPORARY_REDIRECT, nextUrl);
     }
 
     return function (req, res, next) {
