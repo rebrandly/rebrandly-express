d507cefd589743a505cae953f020fdaad6c452f6
spinlud <ludovico.fabbri@gmail.com>
2022-02-03 17:00:53 +0100

    Middleware integration tests

--- src/tests/express-middleware.test.ts
+++ src/tests/express-middleware.test.ts
@@ -1,56 +1,56 @@
+import express from 'express';
+import supertest from 'supertest';
 import { Request, Response } from 'express';
 import { createExpressMiddleware } from '../index';
 
 describe('Rebrandly middleware', () => {
     jest.setTimeout(60000);
 
-    it('should return a 302-redirect to a transformed URL when matching', () => {
+    it('should return a 302-redirect to a transformed URL when matching', async () => {
+        const app = express();
+        const request = supertest(app);
         const alias = "alias.rebrandly.cc";
+        const path = '/demo';
+        const rbMiddleware = createExpressMiddleware({ alias });
 
-        const rbMiddleware = createExpressMiddleware({
-            alias,
-        });
+        app.use(rbMiddleware);
+        app.get(path, (req, res) => res.send('OK'));
 
-        const noOp = () => {
-            return new Error('has not performed any action');
-        };
+        const res = await request.get(path);
+        const { statusCode, redirect, headers } = res;
 
-        const wrongOp = () => {
-            return new Error('has returned a non-302 response to the client');
-        }
-
-        const expectedOp = (statusCode: number, url: string) => {
-            expect(statusCode).toBe(302);
-            expect(url).toBe(`https://${alias}/demo?rb.routing.mode=aliasing`);
-        };
-
-        const req = { path: '/demo' } as unknown as Request;
-        const res = { redirect: expectedOp, json: wrongOp } as unknown as Response;
-        const next = noOp;
-
-        rbMiddleware(req, res, next);
+        expect(statusCode).toBe(302);
+        expect(redirect).toBe(true);
+        expect(headers.location).toBe(`https://${alias}/demo?rb.routing.mode=aliasing`);
     });
 
-    it('should execute with no-op when not matching', () => {
+    it('should execute with no-op when not matching', async () => {
+        const app = express();
+        const request = supertest(app);
+        const path = '/';
+
         const rbMiddleware = createExpressMiddleware({
             skip: true,
         });
 
-        const noOp = () => {};
+        app.use(rbMiddleware);
+        app.get(path, (req, res) => res.send('OK'));
 
-        const op = () => {
-            return new Error('has returned to client rather than proceeding');
-        };
+        const res = await request.get(path);
+        const { statusCode, redirect, headers } = res;
 
-        const req = {} as unknown as Request;
-        const res = { redirect: op, json: op } as unknown as Response;
-        const next = noOp;
-
-        rbMiddleware(req, res, next);
+        expect(statusCode).toBe(200);
+        expect(redirect).toBe(false);
     });
 
-    it('should run only print a message when running in dry mode', () => {
+    it('should run only print a message when running in dry mode', async () => {
+        const app = express();
+        const request = supertest(app);
+        const alias = "alias.rebrandly.cc";
+        const path = '/';
+
         const rbMiddleware = createExpressMiddleware({
+            alias,
             dryRun: true,
             log: (text) => {
                 expect(text).toBeDefined();
@@ -58,34 +58,30 @@ describe('Rebrandly middleware', () => {
             },
         });
 
-        const noOp = () => {};
+        app.use(rbMiddleware);
+        app.get(path, (req, res) => res.send('OK'));
 
-        const op = () => {
-            return new Error('has returned to client rather than proceeding');
-        };
+        const res = await request.get(path);
+        const { statusCode, redirect, headers } = res;
 
-        const req = {} as unknown as Request;
-        const res = { redirect: op, json: op } as unknown as Response;
-        const next = noOp;
-
-        rbMiddleware(req, res, next);
+        expect(statusCode).toBe(200);
+        expect(redirect).toBe(false);
     });
 
-    it('should prevent redirection loops from happening when no branded link is found in Rebrandly', () => {
-        const rbMiddleware = createExpressMiddleware();
+    it('should prevent redirection loops from happening when no branded link is found in Rebrandly', async () => {
+        const app = express();
+        const request = supertest(app);
+        const alias = "alias.rebrandly.cc";
+        const path = '/';
+        const rbMiddleware = createExpressMiddleware({ alias });
 
-        const noOp = () => {};
+        app.use(rbMiddleware);
+        app.get(path, (req, res) => res.send('OK'));
 
-        const op = () => {
-            return new Error('has returned to client rather than proceeding');
-        };
+        const res = await request.get(path).query({ 'rb.routing.mode': 'aliasing' })
+        const { statusCode, redirect, headers } = res;
 
-        const specialQueryParameters = { 'rb.routing.mode': 'aliasing' };
-        const req = { query: specialQueryParameters } as unknown as Request;
-        const res = { redirect: op, json: op } as unknown as Response;
-        const next = noOp;
-
-        rbMiddleware(req, res, next);
-    })
-
+        expect(statusCode).toBe(200);
+        expect(redirect).toBe(false);
+    });
 });
